5aa178b260ddd22b8d3df9244b3d3d83ac7bb3da
Ezra Hill <ezrahills@gmail.com>
2024-07-13 23:13:07 +0100

    Ensure private subnet

--- terraform/vcn.tf
+++ terraform/vcn.tf
@@ -33,12 +33,13 @@ resource "oci_core_subnet" "tf-public-subnet" {
 }
 
 resource "oci_core_subnet" "tf-private-subnet" {
-  cidr_block     = "10.0.0.0/24"
-  compartment_id = oci_identity_compartment.tf-compartment.id
-  display_name   = "sandbox-private-subnet"
-  dns_label      = "sandboxprisnet"
-  vcn_id         = oci_core_vcn.tf-vcn.id
-  route_table_id = oci_core_route_table.tf-private-rt.id
+  cidr_block                 = "10.0.0.0/24"
+  compartment_id             = oci_identity_compartment.tf-compartment.id
+  display_name               = "sandbox-private-subnet"
+  dns_label                  = "sandboxprisnet"
+  vcn_id                     = oci_core_vcn.tf-vcn.id
+  route_table_id             = oci_core_route_table.tf-private-rt.id
+  prohibit_public_ip_on_vnic = true
 }
 
 resource "oci_core_security_list" "tf-public-sl" {
